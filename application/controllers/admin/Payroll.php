12dbb885d7cc6529471ac335aa4caf9db25dae99
Shakilsss <kils674@gmail.com>
2023-02-14 14:42:18 +0600

    changed

--- application/controllers/admin/Payroll.php
+++ application/controllers/admin/Payroll.php
@@ -65,7 +65,7 @@ class Payroll extends MY_Controller {
     	// dd($sql);
 
     	$process_month = date("Y-m-d", strtotime($process_month));
-		$this->Salary_model->salary_process($process_month, $emp_id, $status);
+		$this->Salary_model->Salary_process($process_month, $emp_id, $status);
 		$this->db->trans_complete();
 			
 		if ($this->db->trans_status() === FALSE)
@@ -79,6 +79,31 @@ class Payroll extends MY_Controller {
 		}
 
     }
+
+	public function modify_salary(){
+
+		if(isset($_GET['id'])){
+			$id= $_GET['id'];
+			$data = $this->Xin_model->modify_salary($id);
+			echo json_encode($data);
+		}
+
+	}
+	public function save_modify_salary(){
+
+		
+
+		if(isset($_POST['btn'])){
+
+			// $id= $_POST['id'];
+			// $basic_salary= $_POST['basic_salary'];
+			// $modify_salary= $_POST['modify_salary'];
+
+			// $data = $this->Xin_model->update_salary($id,$basic_salary,$modify_salary);
+			// echo json_encode($data);
+		}
+
+	}
 
 	
 	 // payroll templates
