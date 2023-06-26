80a9ce70e0df78f6fc3125e07d16128d5df5b6d9
Mohammad Shakil Hossain <kils674@gmail.com>
2023-06-26 11:13:08 +0600

    accessory

--- application/models/Accessories_model.php
+++ application/models/Accessories_model.php
@@ -33,12 +33,13 @@ public function get_cat_model_info($id=null){
 }
 
 
-public function get_product_info($id=null){
+public function get_product_reports_info($id=null,$status=null,$category=null){
+    // dd($category);
 
     $this->db->select(' 
                         product_accessories.*,
-                        product_accessory_categories.cat_name,
-                        product_accessory_categories.cat_short_name,
+                        product_accessory_categories.*,
+                        product_accessory_categories.*,
                         product_accessories_model.model_name,
                         mobile_numbers.number,
                         xin_employees.first_name,
@@ -47,49 +48,28 @@ public function get_product_info($id=null){
                     ');
     $this->db->from('product_accessories');
     $this->db->join('product_accessories_model','product_accessories.device_model = product_accessories_model.id','left');
-    $this->db->join('product_accessory_categories','product_accessories.cat_id       = product_accessory_categories.id','left');
-    $this->db->join('mobile_numbers','product_accessories.number       = mobile_numbers.id','left');    
-    $this->db->join('xin_employees','product_accessories.user_id       = xin_employees.user_id','left');
-
-
+    $this->db->join('product_accessory_categories','product_accessories.cat_id    = product_accessory_categories.id','left');
+    $this->db->join('mobile_numbers','product_accessories.number                  = mobile_numbers.id','left');    
+    $this->db->join('xin_employees','product_accessories.user_id                  = xin_employees.user_id','left');
 
     if($id !=null){
         $this->db->where('product_accessories.id',$id);
         $data=$this->db->get()->row();
-    } else {
-
+    } 
+    else if($status !=null && $category!=null){
+        $this->db->where('product_accessories.status',$status);
+        $this->db->where('product_accessories.cat_id',$category);
+        // $this->db->group_by('product_accessories.cat_id');
+        $data=$this->db->get()->result();
+         
+    } 
+    else {
         $data=$this->db->get()->result();
     }
+   
     return $data;          
 }
 
-
-public function get_product_infos($id=null){
-    $this->db->select(' 
-                        product_accessories.*,
-                        product_accessory_categories.cat_name,
-                        product_accessory_categories.cat_short_name,
-                        product_accessories_model.model_name,
-                        mobile_numbers.number,
-                        xin_employees.first_name,
-                        xin_employees.last_name,
-                    ');
-    $this->db->from('product_accessories');
-    $this->db->join('product_accessories_model','product_accessories.device_model = product_accessories_model.id','left');
-    $this->db->join('product_accessory_categories','product_accessories.cat_id       = product_accessory_categories.id','left');
-    $this->db->join('xin_employees','product_accessories.user_id       = xin_employees.user_id','left');
-    $this->db->join('mobile_numbers','product_accessories.number       = mobile_numbers.id','left');
-
-    if($id !=null){
-        $this->db->where('product_accessories.id',$id);
-        $data=$this->db->get()->row();
-    } else {
-
-        $data=$this->db->get()->result();
-    }
-    // echo $this->db->last_query();    dd($data);
-    return $data;          
-}
 
 
 }
--- application/views/admin/accessories/reports.php
+++ application/views/admin/accessories/reports.php
@@ -1,98 +1,101 @@
 
 <?php 
-// dd($row);
 $session = $this->session->userdata('username');
-$eid = $this->uri->segment(4);
 $get_animate = $this->Xin_model->get_content_animate();
 $role_resources_ids = $this->Xin_model->user_role_resource(); 
 ?>
 
 <div class="box <?php echo $get_animate;?>">
-<div class="col-lg-8">
-
+<div class="col-lg-12">
 
 <div id="loader" align="center" style="margin:0 auto; width:600px; overflow:hidden; display:none; margin-top:10px;"><img src="http://localhost/smart-hrm//uploads/ajax-loader.gif"></div>
 
 <div class="box animated fadeInRight">
   <div class="box-header with-border" id="report_title">
     <h3 class="box-title" id="report">Report</h3>
-    <!-- <div class="col-md-4" style="float:right">
-    <div class="form-group" >
-        <select class="form-control" name="status" id="status">
-        <option value="">Select Status</option>
-        <option value="1">regular</option>
-        <option value="2">left</option>
-        <option value="3">resign</option>
-        </select>
-    </div>
-    </div> -->
 </div>
   <div class="box-body" id="emp_report">
-    <ul class="nav nav-tabs " id="myTab" role="tablist">
-        <li class="nav-item active">
-          <a class="nav-link " id="working-tab" data-toggle="tab" href="#working" role="tab" aria-controls="working" aria-selected="true">On Working</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" id="stock-tab" data-toggle="tab" href="#stock" role="tab" aria-controls="stock" aria-selected="false">Stock</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" id="servising-tab" data-toggle="tab" href="#servising" role="tab" aria-controls="servising" aria-selected="false">Servising</a>
-        </li>
-    </ul>
-    
-    <div class="tab-content" id="myTabContent">
-
-        <div class="tab-pane fade active in" id="working" role="tabpanel" aria-labelledby="working-tab" style="margin-top: 30px;">
-            <button class="btn btn-info btn-sm mt-2" style="margin-right:5px" onclick="inventory_report('all')">All Employee Report</button>
-          <!-- <button class="btn btn-sm mr-5 btn-success" onclick="latecomment('latecomment')">Id Wise Report</button> -->
+    <div class="tab-content" id="">
+          <div class="col-md-3">
+            <div class="form-group">
+              <label for="process_date">First Date</label>
+              <input class="form-control attendance_date" placeholder="<?php echo $this->lang->line('xin_select_date');?>" id="first_date" name="first_date" type="text" value="<?php echo date('Y-m-d');?>">
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="form-group">
+              <label for="process_date">Second Date</label>
+              <input class="form-control attendance_date" placeholder="<?php echo $this->lang->line('xin_select_date');?>" id="second_date" name="second_date" type="text" autocomplete="off">
+            </div>
+          </div>
+            <div class="col-md-3"  >
+              <label for="Status">Select Status</label>
+              <select class="form-control" id='status'>  
+                <option value="" disabled selected>Select Status</option>
+                <option value="1" >On Working</option>
+                <option value="2" >Store</option>
+                <option value="3" >Servicing</option>
+                <option value="4" >Destroy</option>
+              </select>
+            </div>
+            <div class="col-md-3"  >
+              <label for="Status">Select Category</label>
+              <select class="form-control" id='category'>  
+                <option value="" disabled selected>Select Category</option>
+            <?php $category = $this->db->select('*')->get('product_accessory_categories')->result(); foreach($category as $row){?>
+                <option value="<?= $row->id?>" ><?= $row->cat_name?></option>
+            <?php }?>
+              </select>
+            </div> 
+            <button class="btn btn-info"  style="float:right;margin-right:15px;margin-top:20px" onclick="inventory_report()">Report</button>
+          </div>
         </div>
-
-        <div class="tab-pane fade" id="stock" role="tabpanel" aria-labelledby="stock-tab" style="margin-top: 30px;">
-            <!-- <button class="btn btn-sm btn-danger"> Button one</button>-->
-            <button class="btn btn-sm mr-5 sbtn mt-2" onclick="monthly_report()">Monthly Register Report</button> 
-        </div>
-
-        <div class="tab-pane fade" id="servising" role="tabpanel" aria-labelledby="servising-tab" style="margin-top: 30px;">
-            <button class="btn btn-sm mr-5 sbtn mt-2" onclick="jobCard()">Job Card</button>
-        </div>
-
-    </div>
-
-  </div>
-
 </div>
 </div>
 
-
-<div class="col-lg-4">
-<div class="box" style="height: 74vh;overflow-y: scroll;">
-<table class="table table-striped table-hover" id="fileDiv">
-  <tbody><tr style="position: sticky;top: 0;z-index:1">
-      <th class="active" style="width:10%"><input type="checkbox" id="select_all" class="select-all checkbox" name="select-all"></th>
-      <th class="" style="width:10%;background:#0177bcc2;color:white">Id</th>
-      <th class=" text-center" style="background:#0177bc;color:white">Name</th>
-  </tr>
-</tbody></table>
-</div>
-</div>
 </div>
 
  
 <script>
 
-  $(document).ready(function() {
-    $('#example').DataTable();
-  });
+$(document).ready(function() {
+$('process_date').datepicker();
+
+  $('#example').DataTable();
+});
 
 
 
-    function inventory_report(status)
-    {
-      var ajaxRequest;  // The variable that makes Ajax possible!
-      ajaxRequest = new XMLHttpRequest();
+function inventory_report(status)
+{
+  var ajaxRequest;  // The variable that makes Ajax possible!
+  ajaxRequest     = new XMLHttpRequest();
+  var first_date  = $('#first_date').val();
+  var second_date = $('#second_date').val();
+
+  var status      = $('#status').val();
+  var category    = $('#category').val();
+
+  if(status==null){
+    alert('Please select status');return;
+  }
+  if(category==null){
+    alert('Please select categroy');return;
+  }
+
+
+  // var statuss = $('#status').find(":selected").text();  
+  // var servicing = statuss.search(/servicing/i); 
+  // var destroy = statuss.search(/destroy/i); 
+  // if(servicing !== -1 || destroy!=-1){
+  //   if(first_date ==null || second_date ==null){
+  //      alert('Please select first date and second date');
+  //   }else{ alert('ok')}
+  // }
+
     
       // console.log(data); return;
-       var data = "status="+status;
+      var data = "status="+status+'&category='+category;
       url = base_url + "/inventory_report";
       ajaxRequest.open("POST", url, true);
       ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
@@ -101,10 +104,12 @@ $role_resources_ids = $this->Xin_model->user_role_resource();
       ajaxRequest.onreadystatechange = function(){
         if(ajaxRequest.readyState == 4){
           var resp = ajaxRequest.responseText;
-          a = window.open('', '_blank', 'menubar=1,resizable=1,scrollbars=1,width=1000,height=800');
+          a = window.open('', '_blank', 'menubar=1,resizable=1,scrollbars=1,width=1400,height=800');
           a.document.write(resp);
         }
       }
     }
+
+    
 
 </script>
