fed6937e87b83e60e5d3ac68253934b007625743
ahaduzzamana <ahaduzzaman.mysoftheaven@gmail.com>
2023-07-09 15:45:56 +0600

    update m

--- application/controllers/admin/Inventory.php
+++ application/controllers/admin/Inventory.php
@@ -193,7 +193,7 @@ class Inventory extends MY_Controller {
 		if(empty($session)){ 
 			redirect('admin/');
 		}
-	dd($session);
+	dd($session['role_id']);
 		$data['title'] 		 = 'Requsition Details | '.$this->Xin_model->site_title();
 		$data['breadcrumbs'] = 'Requsition Details';
 		// $data['path_url'] 	 = 'inventory';
