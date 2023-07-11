4f53e13f44cc4a26c2fa2ef8b07a67d48da042bb
ahaduzzamana <ahaduzzaman.mysoftheaven@gmail.com>
2023-07-11 10:01:13 +0600

    update m

--- application/views/admin/attendance/employee_attandance.php
+++ application/views/admin/attendance/employee_attandance.php
@@ -22,22 +22,79 @@
         </div>
         <div class="col-md-12 box_heding4">
             <div class=" col-md-6 box_heding_footer">
-              <span style="color: #1F1F1F;text-align: center;font-family: Roboto;font-size: 11px;font-style: normal;font-weight: 400;line-height: 18px;">Break</span>
-              <span style="color: rgba(0, 0, 0, 0.85);text-align: center;font-family: Roboto;font-size: 15px;font-style: normal;font-weight: 500;line-height: 14.4px;">1.00hrs</span>
+                <span
+                    style="color: #1F1F1F;text-align: center;font-family: Roboto;font-size: 11px;font-style: normal;font-weight: 400;line-height: 18px;">Break</span>
+                <span
+                    style="color: rgba(0, 0, 0, 0.85);text-align: center;font-family: Roboto;font-size: 15px;font-style: normal;font-weight: 500;line-height: 14.4px;">1.00hrs</span>
             </div>
             <div class=" col-md-6 box_heding_footer">
-              <span style="color: #1F1F1F;text-align: center;font-family: Roboto;font-size: 11px;font-style: normal;font-weight: 400;line-height: 18px;">Overtime</span>
-              <span style="color: rgba(0, 0, 0, 0.85);text-align: center;font-family: Roboto;font-size: 15px;font-style: normal;font-weight: 500;line-height: 14.4px;">1.00hrs</span>
+                <span
+                    style="color: #1F1F1F;text-align: center;font-family: Roboto;font-size: 11px;font-style: normal;font-weight: 400;line-height: 18px;">Overtime</span>
+                <span
+                    style="color: rgba(0, 0, 0, 0.85);text-align: center;font-family: Roboto;font-size: 15px;font-style: normal;font-weight: 500;line-height: 14.4px;">1.00hrs</span>
             </div>
 
         </div>
 
     </div>
     <div class="col-md-3 box">
-
+        <div class="col-md-12 box_heding">
+            <span class="col-md-6 box_heding_text">Statistics</span>
+        </div>
+        <div class="col-md-11 box2_heding2">
+            <div class="col-md-12">
+                <span class="col-md-6 box2_heding2_text1">Today</span>
+                <span class="col-md-6 box2_heding2_text2">3.45 / 8 hrs</span>
+            </div>
+            <div class="progress col-md-12" style="height: 4px;">
+                <div class="progress-bar progress1" role="progressbar" aria-valuenow="40" aria-valuemin="0"
+                    aria-valuemax="100" style="width:40%">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-11 box2_heding2">
+            <div class="col-md-12">
+                <span class="col-md-6 box2_heding2_text1">This Week</span>
+                <span class="col-md-6 box2_heding2_text2">28 / 40 hrs</span>
+            </div>
+            <div class="progress col-md-12" style="height: 4px;">
+                <div class="progress-bar progress2" role="progressbar" aria-valuenow="40" aria-valuemin="0"
+                    aria-valuemax="100" style="width:50%">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-11 box2_heding2">
+            <div class="col-md-12">
+                <span class="col-md-6 box2_heding2_text1">This Month</span>
+                <span class="col-md-6 box2_heding2_text2">90 / 160 hrs</span>
+            </div>
+            <div class="progress col-md-12" style="height: 4px;">
+                <div class="progress-bar progress3" role="progressbar" aria-valuenow="40" aria-valuemin="0"
+                    aria-valuemax="100" style="width:80%">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-11 box2_heding2">
+            <div class="col-md-12">
+                <span class="col-md-6 box2_heding2_text1">Remaining</span>
+                <span class="col-md-6 box2_heding2_text2">90 / 160 hrs</span>
+            </div>
+            <div class="progress col-md-12" style="height: 4px;">
+                <div class="progress-bar progress4" role="progressbar" aria-valuenow="40" aria-valuemin="0"
+                    aria-valuemax="100" style="width:60%">
+                </div>
+            </div>
+        </div>
     </div>
     <div class="col-md-5 box">
-
+        <div class="col-md-12 box3_heding">
+            <span class="col-md-12 box3_heding_text">Attendance Reports</span>
+        </div>
+        <div class="col-md-12">
+            <div class="col-md-6 box3_heding2">
+                <span class="col-md-12">Attendance</span>
+            </div>
+        </div>
     </div>
 
 </div>
