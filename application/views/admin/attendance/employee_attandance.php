cdae43271b9c56454315b704c31148af355f9f73
ahaduzzamana <ahaduzzaman.mysoftheaven@gmail.com>
2023-07-11 11:46:06 +0600

    update m

--- application/views/admin/attendance/employee_attandance.php
+++ application/views/admin/attendance/employee_attandance.php
@@ -90,11 +90,191 @@
         <div class="col-md-12 box3_heding">
             <span class="col-md-12 box3_heding_text">Attendance Reports</span>
         </div>
-        <div class="col-md-12">
-            <div class="col-md-6 box3_heding2">
-                <span class="col-md-12">Attendance</span>
+        <div class="col-md-12" style="display: flex;flex-wrap: wrap;gap: 17px;">
+            <div class="box3_heding2" style="background: #d1ecf1;">
+                <span class="col-md-12 box3_heding2_text1">Working Day</span>
+                <span class="col-md-12 box3_heding2_text2">30</span>
+            </div>
+            <div class="box3_heding2" style="background: #d2f9ee;">
+                <span class="col-md-12 box3_heding2_text1">Active Day</span>
+                <span class="col-md-12 box3_heding2_text2">22</span>
+            </div>
+            <div class=" box3_heding2" style="background: #d6d8d9;">
+                <span class="col-md-12 box3_heding2_text1">Absent</span>
+                <span class="col-md-12 box3_heding2_text2">01</span>
+            </div>
+            <div class="box3_heding2" style="background: #fddcdf;">
+                <span class="col-md-12 box3_heding2_text1">Late Days</span>
+                <span class="col-md-12 box3_heding2_text2">22</span>
+            </div>
+            <div class=" box3_heding2" style="background: #f1cfee;">
+                <span class="col-md-12 box3_heding2_text1">Paid Leave</span>
+                <span class="col-md-12 box3_heding2_text2">12</span>
+            </div>
+            <div class="box3_heding2" style="background: #fff2d8;">
+                <span class="col-md-12 box3_heding2_text1">Unpaid Leave</span>
+                <span class="col-md-12 box3_heding2_text2">01</span>
             </div>
         </div>
     </div>
 
 </div>
+<div class="col-md-12 medelbar">
+    <div class="col-md-3 divform-group">
+        <div class="input">
+            <div class="level">Select Date</div>
+            <div class="pseudo6">
+                <input style="width: 98%;border: none;cursor: pointer;" type="date" name="" value="<?= date('Y-m-d') ?>" id="">
+            </div>
+        </div>
+
+    </div>
+    <div class="col-md-3 divform-group">
+        <div class="input">
+            <div class="level">Select Month</div>
+            <div class="pseudo6">
+                <input style="width: 98%;border: none;cursor: pointer;" type="month" value="<?= date('Y-m') ?>" name="" id="">
+            </div>
+        </div>
+
+    </div>
+    <div class="col-md-3 divform-group">
+        <div class="input">
+            <?php $years = range(1900, strftime("%Y", time())); ?>
+
+            <div class="level">Select Year</div>
+            <div class="pseudo6">
+                <select style="width: 98%;border: none;cursor: pointer;">
+                    <option>Select Year</option>
+                    <?php foreach($years as $year) : ?>
+                    <option <?= ($year=date('Y'))?'selected':'' ?> value="<?php echo $year; ?>"><?php echo $year; ?>
+                    </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+        </div>
+
+    </div>
+    <div class="col-md-3 divform-group">
+        <a>
+            <div class="input serceb">
+                Search
+            </div>
+        </a>
+
+    </div>
+</div>
+<table class="table table-striped" style="border-top: 1px solid #d6d2d2;">
+    <thead>
+      <tr>
+        <th>Sl</th>
+        <th>Date</th>
+        <th>Punch In</th>
+        <th>Punch Out</th>
+        <th>Late</th>
+        <th>Production</th>
+        <th>Break</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>19 Feb 2019</td>
+        <td>10 : 00 AM</td>
+        <td>7 : 00 PM</td>
+        <td>00:00</td>
+        <td>9 hrs</td>
+        <td>1 hrs</td>
+      </tr>
+    </tbody>
+  </table>
