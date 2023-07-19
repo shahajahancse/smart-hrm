db6dc07ae0bbba3b70b44444b30e83e8300dfdb0
Mohammad Shakil Hossain <kils674@gmail.com>
2023-07-19 21:33:00 +0600

    changed

---
+++ application/views/admin/inventory/requip_card.php
@@ -0,0 +1,21 @@
+<div id="card_list">
+    <div class="divrow col-md-12" style="margin-bottom: 27px;margin-top: -15px!important;">
+        <div class="divstats-info col-md-3" style="background-color: #d1ecf1;">
+            <div class="h4">Total Using Items</div>
+            <div class="h4"><?= 0 ?></div>
+        </div>
+
+        <div class="divstats-info col-md-3" style="background-color: #F1CFEE;">
+            <div class="h4">Total Requisition</div>
+            <div class="heading2"><?= 0  ?></div>
+        </div>
+        <div class="divstats-info col-md-3" style="background-color: #E5E5E5;">
+            <div class="h4">Return Items</div>
+            <div class="h4"><?=  0 ?></div>
+        </div>
+        <div class="divstats-info col-md-3" style="background-color: #D2F9EE;">
+            <div class="h4">Pending Items</div>
+            <div class="h4"><?= 0 ?></div>
+        </div>
+    </div>
+</div>
