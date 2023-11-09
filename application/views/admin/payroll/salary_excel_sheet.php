1a67b9467511ed4dba296ced8675db91f552a874
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2023-11-09 09:36:26 +0600

    Some Changes From Apon

--- application/views/admin/payroll/salary_excel_sheet.php
+++ application/views/admin/payroll/salary_excel_sheet.php
@@ -49,8 +49,8 @@ header('Cache-Control: max-age=0'); // No cache
 							<th  class='bnb' style="width: 28px;">D.A Day</th>
 							<th  class='bnb' style="width: 42px;">D.A Salary</th>
 							<th  class='bnb ' style="width: 60px;">Grand Net Salary</th>
-							<!-- <th  class='bnb' style="width: 37px;">Account Number</th>
-							<th  class='bnb' style="width: 37px;">Remark</th> -->
+							<th  class='bnb' style="width: 37px;">Account Number</th>
+							<th  class='bnb' style="width: 37px;">Remark</th>
                         </tr>
                         <tr>
                             <th  class='bnt' colspan="5"></th>
@@ -98,7 +98,7 @@ header('Cache-Control: max-age=0'); // No cache
                                     </tr>
                                 </table>
                             </th>
-                            <th  class='bnt' colspan="5"></th>
+                            <th  class='bnt' colspan="7"></th>
                             <!-- <th  class='bnt'></th>
                             <th  class='bnt'></th>
                             <th  class='bnt'></th>
@@ -169,6 +169,8 @@ header('Cache-Control: max-age=0'); // No cache
 								<td><?=$values[$i]->modify_salary?></td>
 
 								<td class='colors4'><?=$values[$i]->grand_net_salary + $values[$i]->modify_salary - $values[$i]->aba_deduct?></td>
+								<td><?=$values[$i]->account_number?></td>
+								<td></td>
 						</tr>
 
 						<?php } ?>
