661438a3ea00d0bb37b3f7ab1344a9fc8e1455e8
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2023-12-11 14:12:29 +0600

    apon

--- application/controllers/admin/Attendance.php
+++ application/controllers/admin/Attendance.php
@@ -586,6 +586,7 @@ class Attendance extends MY_Controller
 
     $this->db->order_by('xin_attendance_time.clock_in', "ASC");
     $this->db->group_by('xin_attendance_time.employee_id');
+    
 
      $data["values"] = $this->db->get()->result();
 
