ff3f0849213df15a30b06d73d5f53da88dfecadc
Shakilsss <145424838+Shakilssss@users.noreply.github.com>
2025-07-29 13:09:01 +0600

    changed

--- application/config/database.php
+++ application/config/database.php
@@ -74,11 +74,11 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'mysql:host=localhost;dbname=hrm',
+	'dsn'	=> 'mysql:host=localhost;dbname=admin_smarthr',
 	'hostname' => "localhost",
 	'username' => "root",
 	'password' => "",
-	'database' => "hrm",
+	'database' => "admin_smarthr",
 	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
